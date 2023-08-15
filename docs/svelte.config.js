1a1fc05f949978a0fa960f948781b0f41d590909
Abby <i@abby.md>
2023-08-16 03:10:33 +0900

    fix build config

--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -1,6 +1,8 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
